4987b8eff60318a00b802cd8c730664c3b7d87c1
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-03-11 19:38:13 +0545

    Created student input model

--- counseling/models.py
+++ counseling/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class StudentInput(models.Model):
+    name = models.CharField(max_length=100)
+    grades = models.IntegerField()
+    interest = models.CharField(max_length=50)
+    subject = models.CharField(max_length=100, blank=True)
+    hobbies = models.TextField(blank=True)
+    traits = models.TextField(blank=True)
+    suggestion = models.TextField()
+
+    def __str__(self):
+        return f"{self.name} - {self.suggestion}"
+
