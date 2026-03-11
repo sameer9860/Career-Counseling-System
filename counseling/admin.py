05bf30e5fe9e9ea4b4a169442f2abc3cb1009c3f
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-03-11 19:39:31 +0545

    Added student input

--- counseling/admin.py
+++ counseling/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from .models import StudentInput
+
+
+@admin.register(StudentInput)
+class StudentInputAdmin(admin.ModelAdmin):
+    list_display = ("name", "grades", "interest", "suggestion")
+    search_fields = ("name", "interest", "suggestion")
+    list_filter = ("interest", "suggestion")
+
+from django.contrib import admin
+
 # Register your models here.
